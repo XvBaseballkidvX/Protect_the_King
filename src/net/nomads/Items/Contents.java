b1fabee0bcb77904f268604bea6c7aa2cbbbb1d7
XvPROTECTEDvX <joshuabetz@ymail.com>
2014-03-23 20:48:35 -0400

    Update
    Small change such little wow

--- src/net/nomads/Items/Contents.java
+++ src/net/nomads/Items/Contents.java
@@ -8,21 +8,21 @@ import org.bukkit.inventory.ItemStack;
  */
 public class Contents {
 
-    ItemStack[] ARCHER_ARMOR = new ItemStack[]{new ItemStack(Material.LEATHER_HELMET)
-    , new ItemStack(Material.LEATHER_CHESTPLATE)
-    , new ItemStack(Material.LEATHER_LEGGINGS)
-    , new ItemStack(Material.LEATHER_BOOTS)};
+    public static ItemStack[] ARCHER_ARMOR = new ItemStack[]{new ItemStack(Material.LEATHER_HELMET),
+    new ItemStack(Material.LEATHER_CHESTPLATE),
+    new ItemStack(Material.LEATHER_LEGGINGS),
+    new ItemStack(Material.LEATHER_BOOTS)};
 
-    ItemStack[] ARCHER_CONTENTS = new ItemStack[]{new ItemStack(Material.BOW)
-    , new ItemStack(Material.ARROW, 32)
-    , new ItemStack(Material.WOOD_SWORD)};
+    public static ItemStack[] ARCHER_CONTENTS = new ItemStack[]{new ItemStack(Material.BOW),
+    new ItemStack(Material.ARROW, 32),
+    new ItemStack(Material.WOOD_SWORD)};
 
-    ItemStack[] KNIGHT_ARMOR = new ItemStack[]{new ItemStack(Material.IRON_HELMET)
-    , new ItemStack(Material.IRON_CHESTPLATE)
-    , new ItemStack(Material.IRON_LEGGINGS)
-    , new ItemStack(Material.IRON_BOOTS)};
+    public static ItemStack[] KNIGHT_ARMOR = new ItemStack[]{new ItemStack(Material.IRON_HELMET),
+    new ItemStack(Material.IRON_CHESTPLATE),
+    new ItemStack(Material.IRON_LEGGINGS),
+    new ItemStack(Material.IRON_BOOTS)};
 
-    ItemStack[] KNIGHT_CONTENTS = new ItemStack[]{new ItemStack(Material.IRON_SWORD)};
+    public static ItemStack[] KNIGHT_CONTENTS = new ItemStack[]{new ItemStack(Material.IRON_SWORD)};
 
 
 }
